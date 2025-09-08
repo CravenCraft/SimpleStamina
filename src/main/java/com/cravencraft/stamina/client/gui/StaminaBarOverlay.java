d42c137b0283761a98282c665460c7622d6f611b
ryan-craven <benjamin.r.craven@gmail.com>
2025-09-07 20:43:23 -0400

    Beginnings of my custom stamina bar. Texture work mostly done.

--- src/main/java/com/cravencraft/stamina/client/gui/StaminaBarOverlay.java
+++ src/main/java/com/cravencraft/stamina/client/gui/StaminaBarOverlay.java
@@ -41,9 +41,9 @@ public class StaminaBarOverlay implements LayeredDraw.Layer {
 //    static final int DEFAULT_IMAGE_WIDTH = 98;
 //    static final int IMAGE_HEIGHT = 21;
 
-    static final int DEFAULT_IMAGE_WIDTH = 71;
+    static final int STAMINA_BAR_INCREMENT_WIDTH = 31;
+    static final int STAMINA_BAR_INCREMENT_HEIGHT = 13;
     static final int XP_IMAGE_WIDTH = 188;
-    static final int IMAGE_HEIGHT = 15;
 
     static final int IMAGE_FILLED_STAMINA_WIDTH = 67;
     static final int IMAGE_FILLED_STAMINA_HEIGHT = 19;
@@ -84,15 +84,19 @@ public class StaminaBarOverlay implements LayeredDraw.Layer {
         int barX = getBarX(anchor, screenWidth) + configOffsetX;
         int barY = getBarY(anchor, screenHeight, Minecraft.getInstance().gui) - configOffsetY;
 
-        int imageWidth = DEFAULT_IMAGE_WIDTH;
-        int imageHeight = IMAGE_HEIGHT;
+        int imageWidth = STAMINA_BAR_INCREMENT_WIDTH;
+        int imageHeight = STAMINA_BAR_INCREMENT_HEIGHT;
         int spriteX = 0;
         int spriteY = 0;
 
+        // TODO: So far, so good. Need to make a method for incrementing, and account for the beginning section, middle sections, and end section.
+        //      By default, there will be one beginning, two middle, and one end. Each section should consist of 25 stamina points, or maybe somewhere later
+        //      down the road a certain fraction of the player's total stamina. A simple for-loop could probably get most of what I want done with the exception
+        //      of maybe the end. Will have to see as I work through it.
         customGraphicsRenderer(guiGraphics, barX, barY, spriteX, spriteY, imageWidth, imageHeight, 256, 256);
         guiGraphics.setColor(0.0f, 1.0f, 0.0f, 1.0f);
         // TODO: Probably want to modify the stamina bar that drains so that the last bit doesn't get removed until the user is at absolute 0 on stamina.
-        customGraphicsRenderer(guiGraphics, barX, barY, spriteX, spriteY + imageHeight, (int) (imageWidth * Math.min((stamina / (double) maxStamina), 1)), imageHeight,  256, 256);
+//        customGraphicsRenderer(guiGraphics, barX, barY, spriteX, spriteY + imageHeight, (int) (imageWidth * Math.min((stamina / (double) maxStamina), 1)), imageHeight,  256, 256);
 
         guiGraphics.setColor(1.0f, 1.0f, 1.0f, 1.0f);
 
@@ -137,26 +141,26 @@ public class StaminaBarOverlay implements LayeredDraw.Layer {
 
     private static int getBarX(Anchor anchor, int screenWidth) {
         if (anchor == Anchor.Center || anchor == Anchor.Hunger) {
-            return screenWidth / 2 - DEFAULT_IMAGE_WIDTH / 2 + (anchor == Anchor.Center ? 0 : HUNGER_BAR_OFFSET);
+            return screenWidth / 2 - STAMINA_BAR_INCREMENT_WIDTH / 2 + (anchor == Anchor.Center ? 0 : HUNGER_BAR_OFFSET);
         }
         else if (anchor == Anchor.TopLeft || anchor == Anchor.BottomLeft) {
             return SCREEN_BORDER_MARGIN;
         }
-        else return screenWidth - SCREEN_BORDER_MARGIN - DEFAULT_IMAGE_WIDTH;
+        else return screenWidth - SCREEN_BORDER_MARGIN - STAMINA_BAR_INCREMENT_WIDTH;
     }
 
     private static int getBarY(Anchor anchor, int screenHeight, Gui gui) {
         if (anchor == Anchor.Hunger) {
-            return screenHeight - (getAndIncrementRightHeight(gui) - 2) - IMAGE_HEIGHT / 2;
+            return screenHeight - (getAndIncrementRightHeight(gui) - 2) - STAMINA_BAR_INCREMENT_HEIGHT / 2;
         }
         if (anchor == Anchor.Center) {
-            return screenHeight - HOTBAR_HEIGHT - (int) (ICON_ROW_HEIGHT * 2.5f) - IMAGE_HEIGHT / 2 - (Math.max(gui.rightHeight, gui.leftHeight) - 49);
+            return screenHeight - HOTBAR_HEIGHT - (int) (ICON_ROW_HEIGHT * 2.5f) - STAMINA_BAR_INCREMENT_HEIGHT / 2 - (Math.max(gui.rightHeight, gui.leftHeight) - 49);
         }
         if (anchor == Anchor.TopLeft || anchor == Anchor.TopRight){
             return SCREEN_BORDER_MARGIN;
         }
 
-        return screenHeight - SCREEN_BORDER_MARGIN - IMAGE_HEIGHT;
+        return screenHeight - SCREEN_BORDER_MARGIN - STAMINA_BAR_INCREMENT_HEIGHT;
     }
 
     private static int getAndIncrementRightHeight(Gui gui) {
